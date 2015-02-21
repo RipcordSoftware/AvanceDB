494c9ec0d7c323a761f85195d51dc21ab6807b3d
Craig Minihan <craig@ripcordsoftware.com>
2015-02-21 18:16:34 +0000

    Added libmicrohttpd and libhttpserver to externals and build them

--- submodules.sh
+++ submodules.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [ ! -d externals/depot_tools ] || [ ! -d externals/couchdb ]; then
+V8_VER=3.31
+LIBMICROHTTPD_VER=0.9.39
+
+if [ ! -d externals/depot_tools ] || [ ! -d externals/couchdb ] || [ ! -d externals/libhttpserver ]; then
 	git submodule init
 	git submodule update
 fi
@@ -10,7 +13,28 @@ if [ ! -d externals/v8 ]; then
 	export PATH=`pwd`/depot_tools:$PATH
 	fetch v8
 	cd v8
-	git checkout branch-heads/3.31
+	git checkout branch-heads/${V8_VER}
 	make native -j 4
+	popd
+fi
+
+if [ ! -d externals/libmicrohttpdi-${LIBMICROHTTPD_VER} ]; then
+	pushd externals
+	curl ftp://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-${LIBMICROHTTPD_VER}.tar.gz -O
+	cd libmicrohttpd-${LIBMICROHTTPD_VER}
+	./configure
+	make -j 4	
+	popd
+fi
+
+if [ ! -d externals/libhttpserver/build ]; then
+	pushd externals/libhttpserver
+	./bootstrap
+	mkdir build
+	cd build
+	export LIBMICROHTTPD_CFLAGS=-I`pwd`/../../libmicrohttpd-${LIBMICROHTTPD_VER}/src/include/
+	export LIBMICROHTTPD_LIBS=-L`pwd`/../../libmicrohttpd-${LIBMICROHTTPD_VER}/src/microhttpd/.libs/
+	../configure --enable-cpp11
+	make -j 4
 	popd
 fi
