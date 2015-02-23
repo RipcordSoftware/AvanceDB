1846c3b008576365134c6e8441280eac58c3ad47
Craig Minihan <craig@ripcordsoftware.com>
2015-02-23 22:43:25 +0000

    First time add of bare avancedb files

---
+++ src/avancedb/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: craig
+ *
+ * Created on 23 February 2015, 22:39
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
