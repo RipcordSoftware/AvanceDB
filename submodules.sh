aab8681386cc15dc3e8f234e49359afc7655aeac
craig <craig@debian>
2015-05-12 01:07:56 +0100

    Removed the CouchDB git reference

--- submodules.sh
+++ submodules.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-V8_VER=3.31
+#V8_VER=3.31
 
-if [ ! -d externals/depot_tools ] || [ ! -d externals/couchdb ]; then
+if #[ ! -d externals/depot_tools ] || \
+   [ ! -d externals/couchdb ]; then
 	git submodule init
 	git submodule update
 fi
 
-if [ ! -d externals/v8 ]; then
-	pushd externals
-	export PATH=`pwd`/depot_tools:$PATH
-	fetch v8
-	cd v8
-	git checkout branch-heads/${V8_VER}
-	make native -j 4
-	popd
-fi
+#if [ ! -d externals/v8 ]; then
+#	pushd externals
+#	export PATH=`pwd`/depot_tools:$PATH
+#	fetch v8
+#	cd v8
+#	git checkout branch-heads/${V8_VER}
+#	make native -j 4
+#	popd
+#fi
