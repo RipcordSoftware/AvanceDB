aaa22d851bceb1f4efaf5351a833ca73b69628ac
craigminihan <craig@ripcordsoftware.com>
2015-05-26 23:13:48 +0100

    RestServer now uses the RequestRouter

--- src/avancedb/rest_server.cpp
+++ src/avancedb/rest_server.cpp
@@ -1,13 +1,26 @@
 #include "rest_server.h"
 
-void RestServer::RouteRequest(rs::httpserver::socket_ptr socket, rs::httpserver::request_ptr request, rs::httpserver::response_ptr response) {
-    auto uri = request->getUri();
-    
-    if (uri == "/_session") {
-        response->setContentType("application/javascript").Send("{\"ok\":true,\"userCtx\":{\"name\":null,\"roles\":[\"_admin\"]},\"info\":{\"authentication_db\":\"_users\",\"authentication_handlers\":[\"oauth\",\"cookie\",\"default\"],\"authenticated\":\"default\"}}");
-    } else if (uri == "/_all_dbs") {            
-        response->setContentType("application/javascript").Send("[]");
-    } else if (uri == "/") {
-        response->setContentType("application/javascript").Send("{\"couchdb\":\"Welcome\",\"uuid\":\"a2db86472466bcd02e84ac05a6c86185\",\"version\":\"1.6.1\",\"vendor\":{\"version\":\"1.6.1\",\"name\":\"The Apache Software Foundation\"}}");
-    }
+RestServer::RestServer() {
+    router_.Add("GET", "/_session", boost::bind(&RestServer::GetSession, this, _1, _2, _3));
+    router_.Add("GET", "/_all_dbs", boost::bind(&RestServer::GetAllDbs, this, _1, _2, _3));
+    router_.Add("GET", "/", boost::bind(&RestServer::GetSignature, this, _1, _2, _3));
+}
+
+void RestServer::RouteRequest(rs::httpserver::socket_ptr, rs::httpserver::request_ptr request, rs::httpserver::response_ptr response) {
+    router_.Match(request, response);
+}
+
+bool RestServer::GetSession(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response) {
+    response->setContentType("application/javascript").Send("{\"ok\":true,\"userCtx\":{\"name\":null,\"roles\":[\"_admin\"]},\"info\":{\"authentication_db\":\"_users\",\"authentication_handlers\":[\"oauth\",\"cookie\",\"default\"],\"authenticated\":\"default\"}}");
+    return true;
+}
+
+bool RestServer::GetAllDbs(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response) {
+    response->setContentType("application/javascript").Send("[]");
+    return true;
+}
+
+bool RestServer::GetSignature(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response) {
+    response->setContentType("application/javascript").Send("{\"couchdb\":\"Welcome\",\"uuid\":\"a2db86472466bcd02e84ac05a6c86185\",\"version\":\"1.6.1\",\"vendor\":{\"version\":\"1.6.1\",\"name\":\"The Apache Software Foundation\"}}");
+    return true;
 }
--- src/avancedb/rest_server.h
+++ src/avancedb/rest_server.h
@@ -6,9 +6,16 @@
 class RestServer {
 public:
     
+    RestServer();
     void RouteRequest(rs::httpserver::socket_ptr socket, rs::httpserver::request_ptr request, rs::httpserver::response_ptr response);
     
 private:
+    
+    bool GetSession(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response);
+    bool GetAllDbs(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response);
+    bool GetSignature(rs::httpserver::request_ptr request, const rs::httpserver::RequestRouter::CallbackArgs&, rs::httpserver::response_ptr response);
+    
+    rs::httpserver::RequestRouter router_;        
 
 };
 
