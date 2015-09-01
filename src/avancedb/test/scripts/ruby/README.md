bd20442d65b4e167dd25cea604b21a44c70975de
Simon Stearn <simonstearn@gmail.com>
2015-09-01 15:54:56 +0100

    Added very simple ruby script to load 1000 fake contact detail docs.
    Will create DB if its not there already.  Will delete pre-existing DB.

---
+++ src/avancedb/test/scripts/ruby/README.md
@@ -0,0 +1,6 @@
+# REST testing with very basic CouchDB gem usage
+
+To run the test script make sure you have ruby & ruby gems installed.  then
+
+    $ bundle
+    $ ruby run_tests.rb
---
+++ src/avancedb/test/scripts/ruby/run_tests.rb
@@ -0,0 +1,38 @@
+require 'couchdb'
+require 'faker'
+
+
+# Using localhost
+server = CouchDB::Server.new "localhost", 5984
+database = CouchDB::Database.new server, "contact_test"
+database.delete_if_exists!
+database.create_if_missing!
+
+doc_count = 1000
+
+doc_count.times { |index|
+
+#	if index % 100 == 0 then
+		puts "Created #{index} contacts"
+#	end
+
+
+	doc = CouchDB::Document.new database, 
+		"first_name" => Faker::Name.first_name,
+		"last_name" => Faker::Name.last_name,
+		"company_name" => Faker::Company.name,
+		"catch_phrase" => Faker::Company.catch_phrase,
+		"BS" => Faker::Company.bs,
+		"phone_number" => Faker::PhoneNumber.phone_number,
+		"email" => Faker::Internet.email,
+		"ip_v4_address" => Faker::Internet.ip_v4_address,
+		"ip_v6_address" => Faker::Internet.ip_v6_address
+
+	doc.save
+
+}
+
+puts "Created #{doc_count} contacts"
+puts "Done."
+
+
