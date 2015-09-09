63e8305d39f3a1ac5c79cc3ff27aab98becef6ce
craigminihan <craig@ripcordsoftware.com>
2015-09-09 16:05:17 -0700

    Updated submodules.sh

--- submodules.sh
+++ submodules.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-#V8_VER=3.31
-
-if #[ ! -d externals/depot_tools ] || \
-   [ ! -d externals/couchdb ]; then
-	git submodule init
-	git submodule update
-fi
-
-#if [ ! -d externals/v8 ]; then
-#	pushd externals
-#	export PATH=`pwd`/depot_tools:$PATH
-#	fetch v8
-#	cd v8
-#	git checkout branch-heads/${V8_VER}
-#	make native -j 4
-#	popd
-#fi
+git submodule init && git submodule update
