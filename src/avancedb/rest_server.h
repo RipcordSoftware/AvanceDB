7c99dc3a4be3d575586725767c49a0ad93d0c426
craigminihan <craig@ripcordsoftware.com>
2015-09-17 16:06:01 +0100

    Updated source files with the AGPL header

--- src/avancedb/rest_server.h
+++ src/avancedb/rest_server.h
@@ -1,5 +1,23 @@
+/*
+ *  AvanceDB - an in-memory database similar to Apache CouchDB
+ *  Copyright (C) 2015 Ripcord Software
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef REST_SERVER_H
-#define	REST_SERVER_H
+#define REST_SERVER_H
 
 #include "libhttpserver.h"
 #include "libscriptobject.h"
