61b2c3fc60113bfd068d5692d0962557d08a9035
craigminihan <craig@ripcordsoftware.com>
2016-03-02 00:44:09 +0000

    Updated systemd daemon service definition file

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -8,4 +8,5 @@ fi
 daemon/uninstall.sh && \
 rm -f /usr/local/bin/avancedb && \
 rm -rf /usr/local/var/log/avancedb && \
-rm -rf /usr/local/share/avancedb
+rm -rf /usr/local/share/avancedb && \
+rm -rf /var/run/avancedb
