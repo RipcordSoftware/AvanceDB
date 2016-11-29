86f0224049bff3dc487d30eff214da1e0055d86b
Craig Minihan <craig@ripcordsoftware.com>
2016-11-29 10:28:27 +0000

    Corrected the _COV env var reference

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,5 +1,5 @@
 make CC=${_CC} CXX=${_CXX} LDFLAGS=${_LDFLAGS} -j 2 all && make CC=${_CC} CXX=${_CXX} LDFLAGS=${_LDFLAGS} test || exit $?
 
-if [[ "${COV}" != "" ]]; then
+if [ "${_COV}" != "" ]; then
     ./coverage.sh ${_COV}
 fi
