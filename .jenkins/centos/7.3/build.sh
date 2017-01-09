a66014639ec1e4e4073e39eb8a4b9b35f880f987
craigminihan <craig@ripcordsoftware.com>
2017-01-09 14:24:38 +0000

    Added CentOS 7.3 Jenkins build script [skip ci]

---
+++ .jenkins/centos/7.3/build.sh
@@ -0,0 +1,8 @@
+sudo yum install -y autoconf213 && \
+export SHELL=/bin/bash && \
+export PATH=/usr/lib/ccache:$PATH && \
+mkdir -p ${WORKSPACE}/.ccache && export CCACHE_DIR=${WORKSPACE}/.ccache && \
+mkdir -p ${WORKSPACE}/.npm && export npm_config_cache=${WORKSPACE}/.npm && \
+mkdir -p ${WORKSPACE}/.gem && export GEM_HOME=${WORKSPACE}/.gem && export PATH=$GEM_HOME/bin:$PATH && \
+cd build && \
+make -j4
