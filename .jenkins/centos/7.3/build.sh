98f27593ca36fea0a77ab56784997c813ee09c12
Craig Minihan <craig@ripcordsoftware.com>
2017-01-09 20:57:04 +0000

    sudo didn't know the path to couchdb

--- .jenkins/centos/7.3/build.sh
+++ .jenkins/centos/7.3/build.sh
@@ -4,8 +4,8 @@ export PATH=/usr/lib64/ccache:$PATH && \
 mkdir -p ${WORKSPACE}/.ccache && export CCACHE_DIR=${WORKSPACE}/.ccache && \
 mkdir -p ${WORKSPACE}/.npm && export npm_config_cache=${WORKSPACE}/.npm && \
 mkdir -p ${WORKSPACE}/.gem && export GEM_HOME=${WORKSPACE}/.gem && export PATH=$GEM_HOME/bin:$PATH && \
-sudo couchdb -b && \
+sudo /usr/local/bin/couchdb -b && \
 cd build && \
 make -j4 && \
 make -j4 test && \
-sudo couchdb -d
+sudo /usr/local/bin/couchdb -d
