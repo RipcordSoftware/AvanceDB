8f399fbac4b8b456d4f6fbe08fc648e53a9d325f
craigminihan <craig@ripcordsoftware.com>
2017-01-11 15:59:01 +0000

    Install Ruby 2.2 on Travis

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -9,6 +9,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export LIBRARY_PATH=$(pwd)/boost/lib:$LIBRARY_PATH
     export LD_LIBRARY_PATH=$(pwd)/boost/lib:$LD_LIBRARY_PATH
     export CPLUS_INCLUDE_PATH=$(pwd)/boost/include/:$CPLUS_INCLUDE_PATH
+    ### install Ruby
+    rvm install 2.2
     ### install lcov and coveralls
     curl http://ftp.uk.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz -O
     tar xfz lcov_1.11.orig.tar.gz
