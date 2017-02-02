815f86d26cb241a0f8a38f7cde0522b19316e72d
craigminihan <craig@ripcordsoftware.com>
2017-02-02 22:35:09 +0000

    Added a Debian i386 Jenkins build script [skip ci]

---
+++ .jenkins/debian/8.6/i386/build.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y -qq autoconf2.13 && \
+export SHELL=/bin/bash && \
+export PATH=/usr/lib/ccache:$PATH && \
+mkdir -p ${WORKSPACE}/.ccache && export CCACHE_DIR=${WORKSPACE}/.ccache && \
+mkdir -p ${WORKSPACE}/.npm && export npm_config_cache=${WORKSPACE}/.npm && \
+mkdir -p ${WORKSPACE}/.gem && export GEM_HOME=${WORKSPACE}/.gem && export PATH=$GEM_HOME/bin:$PATH && \
+sudo couchdb -b && \
+cd build && \
+make CFLAGS='-m32' CXXFLAGS='-m32' -j4 && \
+make CFLAGS='-m32' CXXFLAGS='-m32' -j4 test && \
+sudo couchdb -d
