2e1925d1f2f8d9a4e84a8d2b9e6ac3a82aeba7bf
craigminihan <craig@ripcordsoftware.com>
2017-02-03 17:03:48 +0000

    Output ccache stats on the i386 Jenkins build [skip ci]

--- .jenkins/debian/8.6/i386/build.sh
+++ .jenkins/debian/8.6/i386/build.sh
@@ -4,8 +4,10 @@ export PATH=/usr/lib/ccache:$PATH && \
 mkdir -p ${WORKSPACE}/.ccache && export CCACHE_DIR=${WORKSPACE}/.ccache && \
 mkdir -p ${WORKSPACE}/.npm && export npm_config_cache=${WORKSPACE}/.npm && \
 mkdir -p ${WORKSPACE}/.gem && export GEM_HOME=${WORKSPACE}/.gem && export PATH=$GEM_HOME/bin:$PATH && \
+echo '********************************' && ccache -s && echo '********************************' && \
 sudo couchdb -b && \
 cd build && \
 make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-linux-gnu' -j4 && \
 make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-linux-gnu' -j4 test && \
-sudo couchdb -d
+sudo couchdb -d && \
+echo '********************************' && ccache -s && echo '********************************'
