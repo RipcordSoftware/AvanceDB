188dfb684dfb4beef37e4a4dd11c3625ba5468ad
Craig Minihan <craig@ripcordsoftware.com>
2017-02-03 11:12:48 +0000

    Updated host type for i386 builds
    [skip ci]

--- .jenkins/debian/8.6/i386/build.sh
+++ .jenkins/debian/8.6/i386/build.sh
@@ -6,6 +6,6 @@ mkdir -p ${WORKSPACE}/.npm && export npm_config_cache=${WORKSPACE}/.npm && \
 mkdir -p ${WORKSPACE}/.gem && export GEM_HOME=${WORKSPACE}/.gem && export PATH=$GEM_HOME/bin:$PATH && \
 sudo couchdb -b && \
 cd build && \
-make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-unknown-gnu-linux' -j4 && \
-make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-unknown-gnu-linux' -j4 test && \
+make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-linux-gnu' -j4 && \
+make CFLAGS='-m32' CXXFLAGS='-m32' MOZJS_CONFIG_FLAGS='--host=i386-linux-gnu' -j4 test && \
 sudo couchdb -d
